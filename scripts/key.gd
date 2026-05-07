a35250c7852ccf51886f1ea741007a763790c6a2
Rasaili-rain <saditrasaili063.com>
2026-05-07 22:06:43 +0545

    walls and pickables collion layer

--- scripts/key.gd
+++ scripts/key.gd
@@ -3,6 +3,8 @@ extends Area2D
 func _ready():
 	body_entered.connect(_on_body_entered)
 
+
+# the key is picked by the player ie key checks for the player
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		GameState.collect_key()
